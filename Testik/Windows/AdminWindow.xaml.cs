50b51dbfba524028683a58fb68a2c2c849aaac58
RuzaL <ruzaloffers@gmail.com>
2023-05-26 23:52:17 +0300

    Добавьте файлы проекта.

---
+++ Testik/Windows/AdminWindow.xaml.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Contexts;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Testik.Windows
+{
+    /// <summary>
+    /// Логика взаимодействия для AdminWindow.xaml
+    /// </summary>
+    public partial class AdminWindow : Window
+    {
+       
+        public AdminWindow(User user)
+        {
+            InitializeComponent();
+            using (TestDemoEntities context = new TestDemoEntities())
+            {
+                TovariList.ItemsSource = context.Tovari.ToList();
+            }
+            userName.Content = "Приветик, " + user.login;
+            nameCombobox.SelectedIndex = 0;
+            
+        }
+
+        private void searchTB_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            using (TestDemoEntities context2 = new TestDemoEntities())
+            {
+                TovariList.ItemsSource = context2.Tovari.Where(x=>x.Name.Contains(searchTB.Text)).ToList();
+            }
+        }
+
+        private void nameCombobox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+           
+            using (TestDemoEntities context3 = new TestDemoEntities())
+            {
+                if (nameCombobox.SelectedIndex==0)
+                {
+                    TovariList.ItemsSource = context3.Tovari.ToList();
+
+                }
+                if (nameCombobox.SelectedIndex == 1)
+                {
+                    TovariList.ItemsSource = context3.Tovari.Where(x=>x.Name.Contains("ruz")).ToList();
+
+                }
+                if (nameCombobox.SelectedIndex == 2)
+                {
+                    TovariList.ItemsSource = context3.Tovari.Where(x => x.Name.Contains("aleg")).ToList();
+
+                }
+                if (nameCombobox.SelectedIndex == 3)
+                {
+                    TovariList.ItemsSource = context3.Tovari.Where(x => x.Name.Contains("agusha")).ToList();
+
+                }
+                if (nameCombobox.SelectedIndex == 4)
+                {
+                    TovariList.ItemsSource = context3.Tovari.Where(x => x.Name.Contains("alexx")).ToList();
+
+                }
+                if (nameCombobox.SelectedIndex == 5)
+                {
+                    TovariList.ItemsSource = context3.Tovari.Where(x => x.Name.Contains("green")).ToList();
+
+                }
+                if (nameCombobox.SelectedIndex == 6)
+                {
+                    TovariList.ItemsSource = context3.Tovari.Where(x => x.Name.Contains("brown")).ToList();
+
+                }
+
+            }
+        }
+
+        private void delButton_Click(object sender, RoutedEventArgs e)
+        {
+            Tovari tov = (Tovari)(sender as Button).DataContext;
+            using (TestDemoEntities context4 = new TestDemoEntities())
+            {
+                var currentTovar = context4.Tovari.FirstOrDefault(x=>x.Id == tov.Id);
+                context4.Tovari.Remove(currentTovar);
+                context4.SaveChanges();
+                TovariList.ItemsSource = context4.Tovari.ToList();
+            }
+
+        }
+    }
+}
---
+++ Testik/Windows/Authentication.xaml.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.IO;
+using EasyCaptcha.Wpf;
+
+namespace Testik.Windows
+{
+    /// <summary>
+    /// Логика взаимодействия для Authentication.xaml
+    /// </summary>
+    public partial class Authentication : Window
+    {
+        public Authentication()
+        {
+            InitializeComponent();
+            MyCaptcha.Visibility = Visibility.Hidden;
+            enterCaptcha.Visibility = Visibility.Hidden;
+            captchaTB.Visibility = Visibility.Hidden;
+          
+        }
+        /// <summary>
+        /// Method for downloading images to database
+        /// </summary>
+        public void LoadImage()
+        {
+            var path = @"E:\images";
+            using (TestDemoEntities context = new TestDemoEntities())
+            {
+                var images = Directory.GetFiles(path);
+                foreach (var tovari in context.Tovari)
+                {
+                    try
+                    {
+                        tovari.Image= File.ReadAllBytes(images.FirstOrDefault(x=>x.Contains(tovari.Name)));
+                    }
+                    catch
+                    {
+                        tovari.Image = File.ReadAllBytes(images.FirstOrDefault(x => x.Contains("pic")));
+                    }
+                }
+                context.SaveChanges();
+            }
+            
+        }
+        
+        /// <summary>
+        /// Method for authorization
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void AuthButton_Click(object sender, RoutedEventArgs e)
+        {
+            using (TestDemoEntities db = new TestDemoEntities())
+            {
+                try
+                {
+                    var user = db.User.FirstOrDefault(x => x.login == LogintT.Text && x.password == PasswordT.Text);
+                    if (user != null && user.RoleId == 1)
+                    {
+                        AdminWindow adw = new AdminWindow(user);
+                        this.Close();
+                        adw.Show();
+
+                    }
+                    else if (user != null && user.RoleId == 2)
+                    {
+                        ManagerWindow mnw = new ManagerWindow(user);
+                        this.Close();
+                        mnw.Show();
+                    }
+                    else
+                    {
+                        MessageBox.Show("Проверьте даныне");
+                        MyCaptcha.Visibility = Visibility.Visible;
+                        enterCaptcha.Visibility = Visibility.Visible;
+                        captchaTB.Visibility = Visibility.Visible;
+                        LogintT.Visibility = Visibility.Hidden;
+                        PasswordT.Visibility = Visibility.Hidden;
+                        AuthButton.Visibility = Visibility.Hidden;
+                        MyCaptcha.CreateCaptcha(Captcha.LetterOption.Alphanumeric, 5);
+                    }
+                   
+                    }
+                catch (Exception)
+                {
+                    MessageBox.Show("Проверьте даныне");
+                }
+
+            }
+                
+        }
+        /// <summary>
+        /// Method for entering captcha
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void enterCaptcha_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                if (MyCaptcha.CaptchaText == captchaTB.Text)
+                {
+                    MyCaptcha.Visibility = Visibility.Hidden;
+                    enterCaptcha.Visibility = Visibility.Hidden;
+                    captchaTB.Visibility = Visibility.Hidden;
+                    LogintT.Visibility = Visibility.Visible;
+                    PasswordT.Visibility = Visibility.Visible;
+                    AuthButton.Visibility = Visibility.Visible;
+                }
+                else
+                {
+                    MessageBox.Show("Неверно");
+                    MyCaptcha.CreateCaptcha(Captcha.LetterOption.Alphanumeric, 5);
+                }
+            }
+
+
+           catch (Exception){
+                MessageBox.Show("Неверно");
+            }
+        }
+    }
+}
